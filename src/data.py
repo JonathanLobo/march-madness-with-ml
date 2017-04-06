eb5b8634edcd27fc88f3aca7f2404aa36166aa01
Jonathan Lobo <jpl68@pitt.edu>
2017-04-06 01:15:14 -0400

    finished data formatting file

--- src/data.py
+++ src/data.py
@@ -1,8 +1,11 @@
 import pandas as pd
+import random
+
+team_stats = {}
+stat_fields = ['score', 'fgm', 'fga', 'fgm3', 'fga3', 'ftm', 'fta', 'or', 'dr', 'ast', 'to', 'stl', 'blk', 'pf']
 
 def format_as_df(csv_file):
-    df = pd.DataFrame.from_csv(csv_file, header=0)
-
+    df = pd.read_csv(csv_file)
     return df
 
 def get_dataframes():
@@ -27,83 +30,86 @@ def update_stats(season, team, fields):
         team_stats[season][team] = {}
 
     for key, value in fields.items():
-        # Make sure we have the field.
+        # Make sure we have the field
         if key not in team_stats[season][team]:
             team_stats[season][team][key] = []
 
-        if len(team_stats[season][team][key]) >= 9:
-            team_stats[season][team][key].pop()
         team_stats[season][team][key].append(value)
 
-def build_season_data(year):
-    team_stats = {}
+def build_season_data(data):
     X = []
     y = []
-    stat_fields = ['score', 'fga', 'fgp', 'fga3', '3pp', 'ftp', 'or', 'dr', 'ast', 'to', 'stl', 'blk', 'pf']
+
+    for index, row in data.iterrows():
+
+        team_1_features = []
+        team_2_features = []
+
+        for field in stat_fields:
+            team_1_stat = get_stat(row['Season'], row['Wteam'], field)
+            team_2_stat = get_stat(row['Season'], row['Lteam'], field)
+            team_1_features.append(team_1_stat)
+            team_2_features.append(team_2_stat)
+
+        if random.random() > 0.5:
+            X.append(team_1_features + team_2_features)
+            y.append(0)
+        else:
+            X.append(team_2_features + team_1_features)
+            y.append(1)
+
+        # Update teams' overall stats so that they can later be averaged and used to make predictions
+        stat_1_fields = {
+            'score': row['Wscore'],
+            'fgm': row['Wfgm'],
+            'fga': row['Wfga'],
+            'fgm3': row['Wfgm3'],
+            'fga3': row['Wfga3'],
+            'ftm': row['Wftm'],
+            'fta': row['Wfta'],
+            'or': row['Wor'],
+            'dr': row['Wdr'],
+            'ast': row['Wast'],
+            'to': row['Wto'],
+            'stl': row['Wstl'],
+            'blk': row['Wblk'],
+            'pf': row['Wpf']
+        }
+        stat_2_fields = {
+            'score': row['Lscore'],
+            'fgm': row['Lfgm'],
+            'fga': row['Lfga'],
+            'fgm3': row['Lfgm3'],
+            'fga3': row['Lfga3'],
+            'ftm': row['Lftm'],
+            'fta': row['Lfta'],
+            'or': row['Lor'],
+            'dr': row['Ldr'],
+            'ast': row['Last'],
+            'to': row['Lto'],
+            'stl': row['Lstl'],
+            'blk': row['Lblk'],
+            'pf': row['Lpf']
+        }
+        update_stats(row['Season'], row['Wteam'], stat_1_fields)
+        update_stats(row['Season'], row['Lteam'], stat_2_fields)
+
+    return X, y
+
+def get_data():
+    year = 2016
+
     for i in range(1985, year+1):
         team_stats[i] = {}
+
     season_detailed_results = format_as_df('../data2016/RegularSeasonDetailedResults.csv')
     tourney_detailed_results = format_as_df('../data2016/TourneyDetailedResults.csv')
     frames = [season_detailed_results, tourney_detailed_results]
     data = pd.concat(frames)
 
-    for index, row in data.iterrows():
-        skip = 0
+    X, Y = build_season_data(data)
 
-        for field in stat_fields:
-            team_1_stat = get_stat(row['Season'], row['Wteam'], field)
-            team_2_stat = get_stat(row['Season'], row['Lteam'], field)
-            if team_1_stat is not 0 and team_2_stat is not 0:
-                team_1_features.append(team_1_stat)
-                team_2_features.append(team_2_stat)
-            else:
-                skip = 1
+    return X, Y, team_stats
 
-        if skip == 0:  # Make sure we have stats.
-            # Randomly select left and right and 0 or 1 so we can train
-            # for multiple classes.
-            if random.random() > 0.5:
-                X.append(team_1_features + team_2_features)
-                y.append(0)
-            else:
-                X.append(team_2_features + team_1_features)
-                y.append(1)
-
-        # AFTER we add the current stuff to the prediction, update for
-        # next time. Order here is key so we don't fit on data from the
-        # same game we're trying to predict.
-        if row['Wfta'] != 0 and row['Lfta'] != 0:
-            stat_1_fields = {
-                'score': row['Wscore'],
-                'fgp': row['Wfgm'] / row['Wfga'] * 100,
-                'fga': row['Wfga'],
-                'fga3': row['Wfga3'],
-                '3pp': row['Wfgm3'] / row['Wfga3'] * 100,
-                'ftp': row['Wftm'] / row['Wfta'] * 100,
-                'or': row['Wor'],
-                'dr': row['Wdr'],
-                'ast': row['Wast'],
-                'to': row['Wto'],
-                'stl': row['Wstl'],
-                'blk': row['Wblk'],
-                'pf': row['Wpf']
-            }
-            stat_2_fields = {
-                'score': row['Lscore'],
-                'fgp': row['Lfgm'] / row['Lfga'] * 100,
-                'fga': row['Lfga'],
-                'fga3': row['Lfga3'],
-                '3pp': row['Lfgm3'] / row['Lfga3'] * 100,
-                'ftp': row['Lftm'] / row['Lfta'] * 100,
-                'or': row['Lor'],
-                'dr': row['Ldr'],
-                'ast': row['Last'],
-                'to': row['Lto'],
-                'stl': row['Lstl'],
-                'blk': row['Lblk'],
-                'pf': row['Lpf']
-            }
-            update_stats(row['Season'], row['Wteam'], stat_1_fields)
-            update_stats(row['Season'], row['Lteam'], stat_2_fields)
-
-    return X, y
+if __name__ == "__main__":
+    trainingX, trainingY, team_stats = get_data()
