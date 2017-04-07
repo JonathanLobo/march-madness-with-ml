4a90d8ad8768ba1ccdf9706ef9e4f05a899b1897
Andrew Levandoski <agl25@pitt.edu>
2017-04-07 12:43:49 -0400

    adds regression and neural net

--- src/neuralnet.py
+++ src/neuralnet.py
@@ -1,5 +1,47 @@
+import data
+import numpy as np
+
 from sklearn.neural_network import MLPClassifier
 
-mlp = MLPClassifier(hidden_layer_sizes=(30,30,30))
-mlp.fix(xTrain, yTrain)
-predictions = mlp.predict(xTest)
+def train():
+    print "Building Data..."
+    trainingX, trainingY, team_stats = data.get_data()
+
+    tourney_teams, team_id_map = data.get_tourney_teams(2017)
+    tourney_teams.sort()
+
+    testingXtemp = []
+
+    matchups = []
+
+    for team_1 in tourney_teams:
+        for team_2 in tourney_teams:
+            if team_1 < team_2:
+                game_features = data.get_game_features(team_1, team_2, 0, 2017, team_stats)
+                testingXtemp.append(game_features)
+
+                game = [team_1, team_2]
+                matchups.append(game)
+
+    testingX = np.array(testingXtemp)
+
+    # here is where you train the model using trainingX and trainingY
+    # then make output label predictions based on testingX
+
+    print "Fitting model..."
+    model = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5, 2), random_state=1)
+    model.fit(trainingX, trainingY)
+
+    print "Generating predictions..."
+    predictions = model.predict(testingX)
+
+    # assuming that predictions is an array of the output labels
+    # name the output file something else
+    for i in range(0, len(matchups)):
+        matchups[i].append(predictions[i])
+
+    results = np.array(matchups)
+    np.savetxt("regression_predictions_2017.csv", results, delimiter=",", fmt='%s')
+
+if __name__ == "__main__":
+    train()
--- src/regression.py
+++ src/regression.py
@@ -1,52 +1,47 @@
-import data as d
+import data
 import numpy as np
-import pandas as pd
 
-from sklearn import cross_validation, linear_model
+from sklearn import linear_model
 
-def predict_winner(team_1, team_2, model, season, stat_fields):
-    features = []
+def train():
+    print "Building Data..."
+    trainingX, trainingY, team_stats = data.get_data()
 
-    # Team 1
-    features.append(get_elo(season, team_1))
-    for stat in stat_fields:
-        features.append(get_stat(season, team_1, stat))
+    tourney_teams, team_id_map = data.get_tourney_teams(2017)
+    tourney_teams.sort()
 
-    # Team 2
-    features.append(get_elo(season, team_2))
-    for stat in stat_fields:
-        features.append(get_stat(season, team_2, stat))
+    testingXtemp = []
 
-    return model.predict_proba([features])
+    matchups = []
 
-def main():
-    year = 2017
-    X, y = d.build_season_data(year)
-    model = linear_model.LogisticRegression()
-    results = []
-
-    print(cross_validation.cross_val_score(
-        model, numpy.array(X), numpy.array(y), cv=10, scoring='accuracy', n_jobs=-1
-    ).mean())
-
-    model.fit(X, y)
-
-    seeds = pd.DataFrame.from_csv('../data2016/TourneySeeds.csv', header=0)
-
-    teams = []
-    for index, row in seeds.iterrows():
-        if row['Season'] == year:
-            teams.append(row['Team'])
-
-    teams.sort()
-    for team_1 in teams:
-        for team_2 in teams:
+    for team_1 in tourney_teams:
+        for team_2 in tourney_teams:
             if team_1 < team_2:
-                prediction = predict_winner(team_1, team_2, model, year, stat_fields)
-                label = str(year) + '_' + str(team_1) + '_' + str(team_2)
-                results.append([label, prediction[0][0]])
-                
-    print results
+                game_features = data.get_game_features(team_1, team_2, 0, 2017, team_stats)
+                testingXtemp.append(game_features)
+
+                game = [team_1, team_2]
+                matchups.append(game)
+
+    testingX = np.array(testingXtemp)
+
+    # here is where you train the model using trainingX and trainingY
+    # then make output label predictions based on testingX
+
+    print "Fitting model..."
+    model = linear_model.LogisticRegression(C=1e5)
+    model.fit(trainingX, trainingY)
+
+    print "Generating predictions..."
+    predictions = model.predict(testingX)
+
+    # assuming that predictions is an array of the output labels
+    # name the output file something else
+    for i in range(0, len(matchups)):
+        matchups[i].append(predictions[i])
+
+    results = np.array(matchups)
+    np.savetxt("neuralnet_predictions_2017.csv", results, delimiter=",", fmt='%s')
 
 if __name__ == "__main__":
-    main()
+    train()
