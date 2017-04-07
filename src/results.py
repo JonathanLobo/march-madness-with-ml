0f2fdd8426d54da93865837f3bf491516cfd4367
Jonathan Lobo <jpl68@pitt.edu>
2017-04-06 21:28:52 -0400

    changed data format and fixed method to get features, added results file, worked on svm

--- src/results.py
+++ src/results.py
@@ -1,4 +1,36 @@
 import data
+import numpy as np
 
 if __name__ == "__main__":
+
     trainingX, trainingY, team_stats = data.get_data()
+
+    tourney_teams, team_id_map = data.get_tourney_teams(2017)
+    tourney_teams.sort()
+
+    testingXtemp = []
+
+    matchups = []
+    matchups.append(['Team1', 'Team2'])
+
+    for team1 in tourney_teams:
+        for team2 in tourney_teams:
+            if team1 < team2:
+                game_features = data.get_game_features(team_1, team_2, 0, 2017, team_stats)
+                testingXtemp.append(game_features)
+
+                game = [team_1, team_2]
+                matchups.append(game)
+
+    testingX = np.array(testingXtemp)
+
+    # here is where you train the model using trainingX and trainingY
+    # then make output label predictions based on testingX
+
+    # assuming that predictions is an array of the output labels
+    # name the output file something else
+    for i in range(0, len(matchups)):
+        matchups[i].append(predictions[i])
+
+    results = np.array(matchups)
+    np.savetxt("ModelName.csv", results, delimiter=",", fmt='%s')
--- src/svm.py
+++ src/svm.py
@@ -1,37 +1,66 @@
-import numpy as np                              # used to support arrays
-from sklearn import svm                         # SVM library
-from sklearn.metrics import accuracy_score      # SVM accuracy metric
+import data
+import numpy as np
+from sklearn import svm
+from sklearn.metrics import accuracy_score
 
-#initialize all kernels
-model_rbf = svm.SVC(kernel='rbf')
-model_linear = svm.SVC(kernel='linear')
-model_poly = svm.SVC(kernel='poly')
-model_sigmoid = svm.SVC(kernel='sigmoid')
+if __name__ == "__main__":
+    trainingX, trainingY, team_stats = data.get_data()
 
-# train all models on training set
-model_rbf.fit(training, np.ravel(trainLabels))
-model_linear.fit(training, np.ravel(trainLabels))
-model_poly.fit(training, np.ravel(trainLabels))
-model_sigmoid.fit(training, np.ravel(trainLabels))
+    print("Generated training set!")
 
-# predict on training set
-predict_rbf_train = model_rbf.predict(training)
-predict_linear_train = model_linear.predict(training)
-predict_poly_train = model_poly.predict(training)
-predict_sigmoid_train = model_sigmoid.predict(training)
+    tourney_teams, team_id_map = data.get_tourney_teams(2017)
+    tourney_teams.sort()
 
-print(str(accuracy_score(trainLabels, predict_rbf_train)))
-print(str(accuracy_score(trainLabels, predict_linear_train)))
-print(str(accuracy_score(trainLabels, predict_poly_train)))
-print(str(accuracy_score(trainLabels, predict_sigmoid_train)))
+    print("Got tourney teams!")
 
-# predict on testing set
-predict_rbf = model_rbf.predict(testing)
-predict_linear = model_linear.predict(testing)
-predict_poly = model_poly.predict(testing)
-predict_sigmoid = model_sigmoid.predict(testing)
+    testingXtemp = []
+    testingYtemp = []
 
-print(str(accuracy_score(testLabels, predict_rbf)))
-print(str(accuracy_score(testLabels, predict_linear)))
-print(str(accuracy_score(testLabels, predict_poly)))
-print(str(accuracy_score(testLabels, predict_sigmoid)))
+    matchups = []
+
+    for team_1 in tourney_teams:
+        for team_2 in tourney_teams:
+            if team_1 < team_2:
+                game_features = data.get_game_features(team_1, team_2, 0, 2017, team_stats)
+                testingXtemp.append(game_features)
+
+                game = [team_1, team_2]
+                matchups.append(game)
+
+    testingX = np.array(testingXtemp)
+    # testingY = np.array(testingYtemp)
+
+    print("Generated testing set!")
+
+    #initialize all kernels
+    model_rbf = svm.SVC(kernel='rbf', probability=True)
+    # model_linear = svm.SVC(kernel='linear', probability=True)
+    # model_poly = svm.SVC(kernel='poly', probability=True)
+    # model_sigmoid = svm.SVC(kernel='sigmoid', probability=True)
+
+    # train all models on training set
+    model_rbf.fit(trainingX, np.ravel(trainingY))
+    # model_linear.fit(trainingX, np.ravel(trainingY))
+    # model_poly.fit(trainingX, np.ravel(trainingY))
+    # model_sigmoid.fit(trainingX, np.ravel(trainingY))
+
+    print("Done fitting SVM!")
+
+    # predict on testing set
+    predict_rbf = model_rbf.predict(testingX)
+    # predict_linear = model_linear.predict(testingX)
+    # predict_poly = model_poly.predict(testingX)
+    # predict_sigmoid = model_sigmoid.predict(testingX)
+
+    print("Finished SVM predictions!")
+
+    for i in range(0, len(matchups)):
+        matchups[i].append(predict_rbf[i])
+
+    results = np.array(matchups)
+    np.savetxt("Predictions2017.csv", results, delimiter=",", fmt='%s')
+
+    # print(str(accuracy_score(testingY, predict_rbf)))
+    # print(str(accuracy_score(testingY, predict_linear)))
+    # print(str(accuracy_score(testingY, predict_poly)))
+    # print(str(accuracy_score(testingY, predict_sigmoid)))
